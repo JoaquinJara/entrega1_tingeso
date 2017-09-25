42097bf37eb75462ae4482b2e433e36b4c107fce
JoaquinJara <joaquin.jara@usach.cl>
2017-09-25 20:12:28 -0300

    add more people in script sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 --Alumnos
 
-insert into alumnos (name, rut, ingreso, carrera, email) values ('nicolas', '19739353-k', 2015, 'Ingenieria', 'nicolas@');
+insert into alumnos (name, rut, ingreso, carrera, email) values ('nicolas', '19739353-k', 2015, 'Ingenieria Civil Informatica', 'nicolas@');
+insert into alumnos (name, rut, ingreso, carrera, email) values ('joaquin', '10845036-2', 2011, 'Ingenieria en Obras Civiles', 'nicolas@');
+insert into alumnos (name, rut, ingreso, carrera, email) values ('catalina', '16082203-3', 2014, 'Ingenieria Mecanica', 'nicolas@');
+insert into alumnos (name, rut, ingreso, carrera, email) values ('fabian', '17888242', 2014, 'Ingenieria Civil Industriual', 'nicolas@');
+insert into alumnos (name, rut, ingreso, carrera, email) values ('rusio', '20782741-0', 2016, 'Ingenieria Civil electrica', 'nicolas@');
