8ef61d09edaffa0d605c1004e369910f4a108626
JoaquinJara <joaquin.jara@usach.cl>
2017-09-25 02:03:44 -0300

    first commit

---
+++ src/main/java/com/example/entrega1_tingeso/Entrega1TingesoApplication.java
@@ -0,0 +1,12 @@
+package com.example.entrega1_tingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Entrega1TingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Entrega1TingesoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/entrega1_tingeso/Entrega1TingesoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.entrega1_tingeso;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Entrega1TingesoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
