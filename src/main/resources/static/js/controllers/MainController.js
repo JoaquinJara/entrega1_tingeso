2b179c7b10569a25647c8828679db1b9ee845ddc
Paredes <nicolas.paredesa@usach.cl>
2017-09-25 15:35:38 -0300

    +Add directories views and controllers

---
+++ src/main/resources/static/js/controllers/MainController.js
@@ -0,0 +1,15 @@
+app.controller('MainController', ['$scope','$location', function($scope,$location) {
+
+	$scope.title = "Ejemplo de pokemon";
+
+    $scope.typePage = $location.path() === '/types';
+    $scope.pokemonPage = $location.path() === '/pokemon';
+    $scope.movementPage = $location.path() === '/movements';
+
+	$scope.navigation = [
+	{"text":"Ver tipos",   "link":"#!/types", "condition":$scope.typePage},
+	{"text":"Ver pokemon",   "link":"#!/pokemon", "condition":$scope.pokemonPage},
+	{"text":"Ver movimientos", "link":"#!/movements", "condition":$scope.movementPage}
+	];
+
+}]);
